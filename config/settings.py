cd57abfbcd1c1247d21f7fc32ae5331cd5a4c3c9
kastle3003 <kastle3003@gmail.com>
2026-06-11 11:33:36 +0530

    fix: disable whitenoise manifest strict for missing source maps

--- config/settings.py
+++ config/settings.py
@@ -106,6 +106,8 @@ TIME_ZONE = 'Asia/Kolkata'
 USE_I18N = True
 USE_TZ = True
 
+WHITENOISE_MANIFEST_STRICT = False
+
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
